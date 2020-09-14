0ba0cc5022235b1f8d9d790b0e7ce3ed01083687
Patrick <patrick.yrkim@gmail.com>
2020-09-13 23:59:57 -0700

    Add error handlers

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// setup error handlers
+const errorHandlers = require('./handlers/errorHandler');
+
+app.use(errorHandlers);
+
 module.exports = app;
