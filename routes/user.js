ed00bca3192ea5131c1f12958f3e6417d5cdb14b
Patrick <patrick.yrkim@gmail.com>
2020-09-15 23:52:58 -0700

    Add routes for login and register

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { catchErrors } = require('../handlers/errorHandlers');
+const userController = require('../controllers/userController');
 
-router.post('/login');
+router.post('/login', catchErrors());
+router.post('/register', catchErrors());
 
 module.exports = router;
