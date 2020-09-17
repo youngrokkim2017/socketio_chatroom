0eabcf601e72a98121f8af14ab442a8648651003
Patrick <patrick.yrkim@gmail.com>
2020-09-16 23:39:06 -0700

    Update router with login and register routes

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { catchErrors } = require('../handlers/errorHandlers');
 const userController = require('../controllers/userController');
 
-router.post('/login', catchErrors());
-router.post('/register', catchErrors());
+router.post('/login', catchErrors(userController.login));
+router.post('/register', catchErrors(userController.register));
 
 module.exports = router;
