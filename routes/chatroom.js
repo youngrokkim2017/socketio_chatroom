a06fa62dc6bb3a53cd590677d2b6e575918bf7c3
Patrick <patrick.yrkim@gmail.com>
2020-09-17 23:51:30 -0700

    Update chat room routes

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { catchErrors } = require('../handlers/errorHandlers');
 const chatroomController = require('../controllers/chatroomController');
+
+const auth = require('../middlewares/auth');
+
+router.post('/', auth, catchErrors(chatroomController.createChatroom));
+
+module.exports = router;
