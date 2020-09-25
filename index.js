1f9d7f45409a79dddad7d65b090824f0fc1b7e6c
Patrick <patrick.yrkim@gmail.com>
2020-09-24 23:59:10 -0700

    Add socket connection to server

--- index.js
+++ index.js
@@ -32,3 +32,27 @@ const app = require('./app');
 const server = app.listen(5000, () => {
     console.log("Server is listening on port 5000")
 })
+
+const io = require('socket.io')(server);
+const jwt = require('jwt-then');
+
+io.use(async (socket, next) => {
+    try {
+        const token = socket.handshake.query.token;
+        const payload = await jwt.verify(token, process.env.SECRET);
+
+        socket.userId = payload.id;
+
+        next();
+    } catch (err) {
+
+    }
+})
+
+io.use('connection', (socket) => {
+    console.log('Connected')
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected')
+    })
+})
