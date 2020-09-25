f5108bc4a8396c5b3cf80ae6f819a468f0fdb7cb
Patrick <patrick.yrkim@gmail.com>
2020-09-24 23:21:05 -0700

    Update server connection

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ require("./models/Message");
 
 const app = require('./app');
 
-app.listen(5000, () => {
-    console.log('Server listening on port 5000');
-});
+// app.listen(5000, () => {
+//     console.log('Server listening on port 5000');
+// });
 
+const server = app.listen(5000, () => {
+    console.log("Server is listening on port 5000")
+})
