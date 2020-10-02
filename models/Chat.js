f88f1add834d92f7db7f22b529c1d0498b53d73f
Patrick <patrick.yrkim@gmail.com>
2020-10-01 23:54:06 -0700

    Add chatroom model

--- models/Chat.js
+++ models/Chat.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
 const Chat = mongoose.model("chat", ChatSchema);
